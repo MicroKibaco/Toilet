4f5d94bba6628ce784fa54095e2e73891cdae3ae
MicroKibaco <yangzy3@asiainfo.com>
2017-04-21 12:11:48 +0800

    服务接口设计

--- service/routes/index.js
+++ service/routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
+
+    /*  return res.send({
+     status: 1,
+     info: '测试服务!!!'
+     });*/
 });
 
 module.exports = router;
