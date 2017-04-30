6a9120e163613592c9c8ea5518553d36f33d92b1
MicroKibaco <yangzy3@asiainfo.com>
2017-04-30 08:54:16 +0800

    推荐专题完成
    Signed-off-by: MicroKibaco <yangzy3@asiainfo.com>

--- teaframe/read/topic.js
+++ teaframe/read/topic.js
@@ -2,7 +2,8 @@
  * 推荐专题
  */
 import React, {Component} from "react";
-import {StyleSheet, Text, View} from "react-native";
+import {Image, StyleSheet, Text, View} from "react-native";
+import Utils from "./../utils";
 
 // A. 实现class的继承
 
@@ -11,8 +12,26 @@ class topic extends Component {
     render() {
 
         return (
-            <View>
-                <Text style={styles.text}>推荐专题 </Text>
+
+
+            <View style={styles.container}>
+                <View>
+                    <Text style={styles.text1}>专题</Text>
+                </View>
+
+                <View style={styles.img_view}>
+                    <View style={styles.img_item}>
+                        <Image resizeMethod='cover' style={styles.img}
+                               source={{uri: 'https://ai.bdstatic.com/dist/1493288513/ai_images/technology/ocr-idcard/demo-card-1.png'} }/>
+                    </View>
+                    <View style={styles.img_item}>
+                        <Image resizeMethod='cover' style={styles.img}
+                               source={{uri: 'http://wiki.mbalib.com/w/images/9/97/%E5%93%81%E7%89%8C%E8%AF%86%E5%88%AB%E7%9A%84%E9%99%B7%E9%98%B1.gif'}}/>
+                    </View>
+                </View>
+                <View>
+                    <Text style={styles.text2}>查看更多同期专题&gt;</Text>
+                </View>
             </View>
         );
 
@@ -22,8 +41,39 @@ class topic extends Component {
 
 const styles = StyleSheet.create({
 
-                                     text: {
-                                         fontSize: 20
+                                     container: {
+
+                                         marginLeft: 10,
+                                         marginRight: 10,
+
+                                     },
+
+                                     img: {
+                                         width: (Utils.size.width - 30) / 2,
+                                         height: 80,
+                                         borderRadius: 5,
+                                     },
+
+                                     img_view: {
+                                         flexDirection: 'row'
+
+                                     },
+                                     img_item: {
+                                         flex: 1
+                                     },
+
+                                     text1: {
+                                         color: '#5E5E5E',
+                                         marginBottom: 8,
+                                         fontSize: 17
+                                     },
+                                     text2: {
+                                         color: '#ccc',
+                                         fontSize: 13,
+                                         marginTop: 10,
+                                         fontWeight: '300',
+                                         marginBottom: 10,
+
                                      }
 
                                  });
